39d98b07a2e81552cd73295abce53bcf7927d8e2
amtkxa <scarlet0211@outlook.jp>
2018-12-23 12:34:55 +0900

    initial commit

---
+++ src/main/java/com/amtkxa/springbootreladomosimple/SpringBootReladomoSimpleApplication.java
@@ -0,0 +1,14 @@
+package com.amtkxa.springbootreladomosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReladomoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReladomoSimpleApplication.class, args);
+	}
+
+}
+
